e9ac8e2b0d4a2da3a8b08412d8b8eb8a4bfed2de
JureFilipovic <jure.filipovic@gmail.com>
2025-05-15 12:24:05 +0200

    Import used module and init the app

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import appControllerModule from "./app-controller-module";
+import domControllerModule from "./dom-controller-module";
+import "./css/weather-icons.min.css"
+
+domControllerModule.init();
