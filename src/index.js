c2d7a01d572d2ef6b1e28ba5db9f3bfea4ed2091
JureFilipovic <jure.filipovic@gmail.com>
2025-05-21 10:17:03 +0200

    Add style.css import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import appControllerModule from "./app-controller-module";
 import domControllerModule from "./dom-controller-module";
 import "./css/weather-icons.min.css"
+import "./css/style.css"
 
 domControllerModule.init();
